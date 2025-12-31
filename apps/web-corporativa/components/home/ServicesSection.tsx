4f61d5061826707665c78ae29f4c246f7b7ac81f
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2025-12-30 19:54:45 -0500

    Actualización: Se corrigio detalle visual.

--- apps/web-corporativa/components/home/ServicesSection.tsx
+++ apps/web-corporativa/components/home/ServicesSection.tsx
@@ -177,7 +177,7 @@ export default function ServicesSection() {
           )}
         </div>
         <div
-          className="max-w-7xl mx-auto px-4 md:px-0 py-15">
+          className="max-w-7xl mx-auto py-15">
           <div
             className="grid grid-cols-1 md:grid-cols-3 gap-4">
             <div
