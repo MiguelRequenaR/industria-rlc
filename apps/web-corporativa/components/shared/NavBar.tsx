6dd17c2af433e56bc17f823602de05cc89c1f4a3
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2026-01-07 23:20:48 -0500

    Actualización: Se modifico un valor en el navbar.

--- apps/web-corporativa/components/shared/NavBar.tsx
+++ apps/web-corporativa/components/shared/NavBar.tsx
@@ -124,7 +124,7 @@ export default function NavBar() {
   return (
     <header className="bg-white fixed top-0 left-0 right-0 z-50 shadow-lg">
       {/* Desktop */}
-      <div className="max-w-7xl mx-auto flex flex-col justify-between items-center py-5 px-4 md:px-0">
+      <div className="max-w-7xl mx-auto flex flex-col justify-between items-center md:py-5 px-4 md:px-0">
         <div className="flex items-center justify-between gap-4 w-full md:w-auto md:justify-start">
           <Link href="/">
             <div className="flex items-center gap-2">
