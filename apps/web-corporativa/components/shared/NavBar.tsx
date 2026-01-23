4f5327c2c3a332cec4f71ce6b52f89f625c45980
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2026-01-23 15:00:23 -0500

    Actualización: Reducción de tamaño de navbar para pantallas laptop.

--- apps/web-corporativa/components/shared/NavBar.tsx
+++ apps/web-corporativa/components/shared/NavBar.tsx
@@ -120,7 +120,7 @@ export default function NavBar() {
   return (
     <header className="bg-white fixed top-0 left-0 right-0 z-50 shadow-lg">
       {/* Desktop */}
-      <div className="max-w-7xl mx-auto flex flex-col justify-between items-center md:py-5 px-4 md:px-0">
+      <div className="max-w-7xl mx-auto flex flex-col justify-between items-center px-4 md:px-0">
         <div className="flex items-center justify-between gap-4 w-full md:w-auto md:justify-start">
           <Link href="/">
             <div className="flex items-center gap-2">
@@ -210,7 +210,7 @@ export default function NavBar() {
             </span>
           </div>
         </div>
-        <div className="flex items-center gap-4 mt-5">
+        <div className="flex items-center gap-4">
           <nav className="hidden md:block">
             <ul className="text-primary flex gap-15 font-semibold">
               {links.map((item) => (
@@ -373,7 +373,7 @@ export default function NavBar() {
         <div className="flex flex-col gap-4 justify-center pb-8 px-5">
           <a href="https://industria-rlc-web-academia.vercel.app/" target="_blank" rel="noopener noreferrer">
             <button
-              className="relative overflow-hidden bg-white text-primary px-8 py-3 uppercase cursor-pointer border-2 border-secondary group transition-colors duration-500 hover:text-white text-base rounded-full w-full"
+              className="relative overflow-hidden bg-white text-primary px-8 py-3 uppercase cursor-pointer group transition-colors duration-500 hover:text-white text-base rounded-full w-full"
             >
               <span
                 className="
