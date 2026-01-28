764e610e2d28f0b82f3dcf56b3765f67f8e2040f
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2026-01-28 15:47:05 -0500

    Actualización: Se agrego los iconos de redes sociales en el pie de página.

--- apps/web-academia/components/shared/Footer.tsx
+++ apps/web-academia/components/shared/Footer.tsx
@@ -34,6 +34,35 @@ export default function Footer() {
             <p className='text-gray-300 text-sm leading-relaxed'>
               Formación técnica de excelencia para el mundo real. Tu futuro comienza aquí.
             </p>
+            <div className='pt-4'>
+              <p>Síguenos en nuestras redes:</p>
+              <div className='flex items-center gap-2 pt-2'>
+                <a
+                  href="https://www.facebook.com/profile.php?id=61586886613195"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Facebook de RLC Academy 360"
+                >
+                  <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2021%2F06%2F15%2F12%2F51%2Ffacebook-6338508_1280.png&f=1&nofb=1&ipt=4f3040e83e629ea57628a2a63207c14be5bd3a3282a7301acd60c3ff6b5236ca" alt="Logo de Facebook de RLC Academy 360" className='w-7 h-7' />
+                </a>
+                <a
+                  href="https://www.tiktok.com/@rlccentrodecapacitacion"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="TikTok de RLC Academy 360"
+                >
+                  <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-world.net%2Fwp-content%2Fuploads%2F2020%2F04%2FTikTok-Logo-2016-present.png&f=1&nofb=1&ipt=8fa3d48e081e27f0f9fc9a0d493039834aef3d3d18f11fb5b808704fb13caf0f" alt="Logo de TikTok de RLC Academy 360" className='h-7' />
+                </a>
+                <a
+                  href="https://www.youtube.com/@rlc-centrodecapacitaciones"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="YouTube de RLC Academy 360"
+                >
+                  <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%2Fid%2FOIP.gXDAsM3eyXW3wlU8IMGrfgHaHa%3Fpid%3DApi&f=1&ipt=ed4bcead4a3d8177f4946392cc7ad1586c3138f83e430ce819866517beb76ef6" alt="Logo de YouTube de RLC Academy 360" className='w-7 h-7 rounded-full' />
+                </a>
+              </div>
+            </div>
           </div>
 
           <div>
